956e242133857c448c95e43d0088e75417991448
caresppen <52134186+caresppen@users.noreply.github.com>
2020-08-19 12:21:18 +0200

    Add files via upload

---
+++ selenium_tutorial.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+browser = webdriver.Firefox(executable_path=r"C:\@Carlos\Data Science\3. Python\Scripts\Tools\selenium\geckodriver\geckodriver.exe")
+
+browser.get('http://www.seleniumhq.org')
+
+elem = browser.find_element_by_link_text('Downloads')
+elem.click()
+searchBar = browser.find_element_by_name("search")
+searchBar.send_keys('download')
+
+# Using Keys to interact with the elements: searchBar
+searchBar.send_keys(Keys.ENTER)
